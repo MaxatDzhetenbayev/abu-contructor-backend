423a807976ea35f13a5696ce2418c7959a04dae6
Yerkow <112828364+yerkow@users.noreply.github.com>
2025-01-09 15:56:57 +0500

    refactor: cleaning config

--- src/sentry.config.ts
+++ src/sentry.config.ts
@@ -1,24 +1,27 @@
-import * as Sentry from '@sentry/nestjs';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
-import * as dotenv from 'dotenv';
+import * as Sentry from "@sentry/nestjs";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import { ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 
-dotenv.config();
+ConfigModule.forRoot({ isGlobal: true });
+const configService = new ConfigService();
 
-const SENTRY_DSN = process.env.SENTRY_DSN;
+const sentryDsn = configService.get<string>("SENTRY_DSN");
 
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: sentryDsn,
   integrations: [nodeProfilingIntegration()],
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 1.0
 });
 
 Sentry.profiler.startProfiler();
 
 Sentry.startSpan(
   {
-    name: 'My First Transaction',
+    name: "My First Transaction"
   },
-  () => {},
+  () => {
+  }
 );
 
 Sentry.profiler.stopProfiler();
