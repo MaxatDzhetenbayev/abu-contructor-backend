c7297a207d2190953645632750268713bf6e663c
Yerkow <112828364+yerkow@users.noreply.github.com>
2025-01-09 15:24:59 +0500

    feat: Sentry logging

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
+import * as Sentry from '@sentry/node';
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import './sentry.config';
 import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+import { SentryExceptionFilter } from './sentry-exception.filter';
+import './sentry.config';
+
+dotenv.config();
+
+const SENTRY_DSN = process.env.SENTRY_DSN;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  Sentry.init({ dsn: SENTRY_DSN });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -19,7 +27,8 @@ async function bootstrap() {
     origin: configService.get<string>('FRONTEND_URL'),
     credentials: true,
   });
-  
+
+  app.useGlobalFilters(new SentryExceptionFilter());
   app.setGlobalPrefix('api');
   await app.listen(3003);
 }
--- src/sentry.config.ts
+++ src/sentry.config.ts
@@ -9,24 +9,16 @@ const SENTRY_DSN = process.env.SENTRY_DSN;
 Sentry.init({
   dsn: SENTRY_DSN,
   integrations: [nodeProfilingIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  tracesSampleRate: 1.0,
 });
 
-// Manually call startProfiler and stopProfiler
-// to profile the code in between
 Sentry.profiler.startProfiler();
 
-// Starts a transaction that will also be profiled
 Sentry.startSpan(
   {
     name: 'My First Transaction',
   },
-  () => {
-    // the code executing inside the transaction will be wrapped in a span and profiled
-  },
+  () => {},
 );
 
-// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
-// your application until the process exits or stopProfiling is called.
 Sentry.profiler.stopProfiler();
