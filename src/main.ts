1a13b5db7b5640ac86a9e494296e7e156c00adc4
Yerkow <112828364+yerkow@users.noreply.github.com>
2025-02-28 21:49:00 +0500

    test: Sentry debug

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-// import * as Sentry from '@sentry/node';
+import * as Sentry from '@sentry/node';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
-// import { SentryExceptionFilter } from './sentry-exception.filter';
+import { SentryExceptionFilter } from './sentry-exception.filter';
 import './sentry.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  // const sentryDsn = configService.get<string>('SENTRY_DSN');
-  // Sentry.init({ dsn: sentryDsn });
+  const sentryDsn = configService.get<string>('SENTRY_DSN');
+  Sentry.init({ dsn: sentryDsn });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -24,7 +24,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // app.useGlobalFilters(new SentryExceptionFilter());
+  app.useGlobalFilters(new SentryExceptionFilter());
   app.setGlobalPrefix('api');
   await app.listen(3003);
 }
